3407075b824cd9ed45c79d386657a4f936f71cf9
fhc <1518265554@qq.com>
2018-10-25 15:18:12 +0800

    a

--- src/views/myAccount/sections/withdraw/withdraw.vue
+++ src/views/myAccount/sections/withdraw/withdraw.vue
@@ -4,24 +4,37 @@
          style='font-size: 30px;margin-bottom: 30px;'>我要出金
     </div>
 
-    <el-form status-icon
-             style='margin-top: 30px;'
-             ref="ruleForm2"
-             label-width="100px"
-             class="demo-ruleForm">
-      <el-form-item label="出金金额"
-                    prop="amount">
-        <el-input type="number"
-                  placeholder="出金金额"
-                  v-model.number="amount"></el-input>
-      </el-form-item>
+    <div v-if='loading' v-loading='loading' style='padding: 30px;'>
+    </div>
+    <div v-else>
+      <div v-if='!realNameFlag'>请先进行实名认证</div>
+      <div v-else-if='!bindCardFlag'>请先绑定银行卡</div>
 
-      <el-form-item>
-        <el-button type="primary"
-                   @click="validForm">确定
-        </el-button>
-      </el-form-item>
-    </el-form>
+      <div v-else>
+        <el-form status-icon
+                 style='margin-top: 30px;'
+                 ref="ruleForm2"
+                 label-width="140px"
+                 class="demo-ruleForm">
+          <el-form-item label="可用资金(USD)"
+                        prop="amount">
+            ￥{{availableBalance}}
+          </el-form-item>
+          <el-form-item label="出金金额"
+                        prop="amount">
+            <el-input type="number"
+                      placeholder="出金金额"
+                      v-model.number="amount"></el-input>
+          </el-form-item>
+
+          <el-form-item>
+            <el-button type="primary"
+                       @click="validForm">确定
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -31,7 +44,12 @@
   export default {
     data() {
       return {
-        amount: ''
+        amount: '',
+        availableBalance: '',
+        bindCardFlag: false,
+        openAccountFlag: false,
+        realNameFlag: false,
+        loading: true
       }
     },
     created() {
@@ -44,15 +62,33 @@
     },
     methods: {
       getToken() {
-        if (!USER.isLogin()) {
+        if (USER.isLogin()) {
+          this.getAccount()
+        } else {
           USER.logout()
           this.$router.push('/login')
         }
+      },
+      getAccount() {
+        AXIOS.post('/api/member/center').then(res => {
+          this.loading = false
+          this.availableBalance = res.availableBalance
+          this.balance = res.balance
+          this.bindCardFlag = res.bindCardFlag
+          this.name = res.name
+          this.openAccountFlag = res.openAccountFlag
+          this.realNameFlag = res.realNameFlag
+          this.account = res.account
+        })
       },
 
       validForm() {
         if (!this.amount) {
           this.$message.warning('请输入出金金额');
+          return
+        }
+        if (this.amount > this.availableBalance) {
+          this.$message.warning('可用资金不足');
           return
         }
         this.doConfirm()
