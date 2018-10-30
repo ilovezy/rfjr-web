ce923b98c22672450acb684249b53a8002a10dad
fhc <1518265554@qq.com>
2018-10-30 15:05:55 +0800

    a

--- src/global/Fetch.js
+++ src/global/Fetch.js
@@ -77,8 +77,12 @@ instance.interceptors.response.use(res => {
     }
   } else {
     Message.error({
-      message: '网络有点卡，请稍后重试'
+      message: '登录信息过期，请登录'
     })
+    USER.logout()
+    setTimeout(() => {
+      location.href = '/web/#/login'
+    }, 2000)
     return Promise.reject(err)
   }
 })
